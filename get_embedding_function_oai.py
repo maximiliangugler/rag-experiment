0c206089574ec07262197a5860902baa83f047f6
maxggl <maximilian.gugler@drei.at>
2024-05-02 14:14:15 +0200

    refactoring

--- get_embedding_function_oai.py
+++ get_embedding_function_oai.py
@@ -5,6 +5,6 @@ from langchain_openai import OpenAIEmbeddings
 def get_embedding_function_oai():
     embeddings = OpenAIEmbeddings(
         model="text-embedding-3-small",
-        api_key="sk-proj-0AeNS5Mz0hSZ4lhqgCXeT3BlbkFJir9RcfcQHDjPmB7aZpe5"
+        api_key="sk-"
     )
     return embeddings
